a2852a5311d24daba105089ed503d427e861bf84
Fco. Javier Clavero <jc.klavman76@gmail.com>
2017-05-09 13:49:13 +0200

    add example 03-const

--- src/03-const.js
+++ src/03-const.js
@@ -4,3 +4,19 @@ console.log(N_ABC);
 N_ABC = 10;
 
 console.log(N_ABC);
+
+
+const USER = {
+	name: 'Charlie',
+	surname: 'Sheen',
+	age: null
+};
+
+USER.name = 'Charles';
+USER.age = 50;
+
+USER.height = 187;
+
+delete USER.age;
+
+console.log(USER);
