67d0c4012333806b4d37ba770d1110925cde2d49
Fco. Javier Clavero <jc.klavman76@gmail.com>
2017-05-11 12:25:20 +0200

    add example export/import

---
+++ modules/app.js
@@ -0,0 +1,7 @@
+import {plus, Rectangle} from './module';
+
+console.log(plus(8, 9));
+
+const rect1 = new Rectangle(8, 20);
+
+console.log(rect1.width);
---
+++ modules/module.js
@@ -0,0 +1,14 @@
+export var color = 'red';
+export let name = 'Charles';
+export const MAGIC_NUMBER = 7;
+
+export function plus(a, b) {
+	return a + b;
+}
+
+export class Rectangle {
+	constructor(length, width) {
+		this.length = length;
+		this.width = width;
+	}
+}
